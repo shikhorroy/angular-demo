4f1edc4f6241a7733fbe6b5a6d5b09d3c2620639
Shikhor Roy <shikhorroy.cse12@gmail.com>
2020-02-01 23:00:36 +0600

    Pass Property Attribute test.

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  @Input()
+  model: any;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
