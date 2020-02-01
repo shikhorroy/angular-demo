4d88be62c1689cb9b0f116e87712b149d82747bb
Shikhor Roy <shikhorroy.cse12@gmail.com>
2020-02-01 10:46:25 +0600

    property binding + ngFor directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,8 +6,19 @@ import {Component} from '@angular/core';
 })
 export class ServerComponent {
   shouldDisabled = true;
+  numList = [1, 2, 3, 4];
 
   constructor() {
     setTimeout(() => this.shouldDisabled = false, 2000);
+  }
+
+  addNumValue() {
+    const x = Math.ceil(Math.random() * 10);
+    this.numList.push(x);
+  }
+
+  getBGColor() {
+    const sz = this.numList.length;
+    return (this.numList[sz - 1] % 2 === 0 ? 'green' : 'transparent');
   }
 }
